8db82bfd799ad24d68c8d2424cd919ab87f833dc
tw <tw@gmail.com>
2026-03-02 10:28:51 +0600

    pms group

--- erpnext/pms/doctype/pms_group/pms_group.py
+++ erpnext/pms/doctype/pms_group/pms_group.py
@@ -7,4 +7,19 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 
 class PMSGroup(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		check: DF.Data | None
+		description: DF.Data | None
+		group_name: DF.Data
+		required_to_set_target: DF.Check
+		weightage_for_competency: DF.Data
+		weightage_for_target: DF.Data
+	# end: auto-generated types
 	pass
