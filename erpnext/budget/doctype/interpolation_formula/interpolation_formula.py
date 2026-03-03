5e739b59f010c89faa714dbb4fe3d3336cb9d673
Lhendup97 <k.lhendup97@gmail.com>
2026-03-03 20:46:05 +0600

    Budget changes

--- erpnext/budget/doctype/interpolation_formula/interpolation_formula.py
+++ erpnext/budget/doctype/interpolation_formula/interpolation_formula.py
@@ -6,4 +6,21 @@ from frappe.model.document import Document
 
 
 class InterpolationFormula(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		apa_max: DF.Float
+		apa_min: DF.Float
+		category: DF.Link | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		pms_max: DF.Float
+		pms_min: DF.Float
+	# end: auto-generated types
 	pass
