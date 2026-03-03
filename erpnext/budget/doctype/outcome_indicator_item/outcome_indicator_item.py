1264d5f7a055a9fb1a71da9abb59d369cf88a8bb
Lhendup97 <k.lhendup97@gmail.com>
2026-03-04 00:25:02 +0600

    Budget changes

--- erpnext/budget/doctype/outcome_indicator_item/outcome_indicator_item.py
+++ erpnext/budget/doctype/outcome_indicator_item/outcome_indicator_item.py
@@ -6,4 +6,21 @@ from frappe.model.document import Document
 
 
 class OutcomeIndicatorItem(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		category: DF.Link | None
+		max: DF.Float
+		min: DF.Float
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		raw_rating: DF.Data | None
+		unit: DF.Data | None
+	# end: auto-generated types
 	pass
